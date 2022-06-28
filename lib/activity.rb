3e31dca0e10acdcec4461070e84a48a2e72c00a5
Nikhil Shahi <shniks@gmail.com>
2022-06-28 08:56:38 -0700

    created tests and method for existence.

---
+++ lib/activity.rb
@@ -0,0 +1,13 @@
+class Activity
+
+  attr_reader :activity 
+
+  def initialize(activity)
+    @activity = activity
+  end
+
+
+
+
+
+end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -7,6 +7,12 @@ require './lib/activity'
 
 class ActivityTest < Minitest::Test
 
+  def test_if_it_exists
+    activity = Activity.new("Brunch")
+
+    assert_instance_of Activity, activity
+  end 
+
 
 
 
