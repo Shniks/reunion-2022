9ed69a46ac38b8f2f94e541a5a7f4b993ed4be4a
Nikhil Shahi <shniks@gmail.com>
2022-06-28 09:14:59 -0700

    Added test to add a participant.

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -20,6 +20,24 @@ class ActivityTest < Minitest::Test
     assert_instance_of Hash, activity.participants
   end
 
+  def test_it_can_add_a_participant
+    activity = Activity.new("Brunch")
+    activity.add_participant("Maria", 20)
+
+    assert_equal ({"Maria" => 20}), activity.participants
+    assert_equal 20, activity.total_cost
+  end
+
+  def test_it_can_add_another_participant
+    activity = Activity.new("Brunch")
+    activity.add_participant("Maria", 20)
+    activity.add_participant("Luther", 40)
+
+    assert_equal ({"Maria" => 20, "Luther" => 40}), activity.participants
+    assert_equal 60, activity.total_cost
+  end
+
+
 
 
 
