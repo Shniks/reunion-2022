0a4d575e023c14b4f63d2596779edeba4ca9a610
Nikhil Shahi <shniks@gmail.com>
2022-06-29 10:41:59 -0700

    Created methods to breakout the amount owed by each participant.

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -3,11 +3,13 @@ require './lib/activity'
 
 class Reunion
   attr_reader :name,
-              :activities
+              :activities,
+              :total_owed
 
   def initialize(name)
     @name = name
     @activities = []
+    @total_owed = Hash.new(0)
   end
 
   def add_activity(activity)
@@ -17,6 +19,19 @@ class Reunion
   def total_cost
     activities.reduce(0) do |total_cost, activity|
       total_cost + activity.total_cost
+    end
+  end
+
+  def breakout
+    activities.each do |activity|
+      owed_by_participant(activity)
+    end
+    @total_owed
+  end
+
+  def owed_by_participant(activity)
+    activity.participants.each do |participant, owed|
+      @total_owed[participant] += activity.owed[participant]
     end
   end
 
