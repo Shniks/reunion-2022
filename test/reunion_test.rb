af69aff46d3e2293cd23d83711149af85d74d4eb
Nikhil Shahi <shniks@gmail.com>
2022-06-29 09:43:34 -0700

    Created test to compute breakout per participant.

--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -54,5 +54,20 @@ class ReunionTest < Minitest::Test
     assert_equal 180, reunion.total_cost
   end
 
+  def test_if_it_can_provide_a_breakout_by_participant
+    reunion = Reunion.new("1406 BE")
+    activity_1 = Activity.new("Brunch")
+    activity_1.add_participant("Maria", 20)
+    activity_1.add_participant("Luther", 40)
+    reunion.add_activity(activity_1)
+    activity_2 = Activity.new("Drinks")
+    activity_2.add_participant("Maria", 60)
+    activity_2.add_participant("Luther", 60)
+    activity_2.add_participant("Louis", 0)
+    reunion.add_activity(activity_2)
+
+    assert_equal ({"Maria" => -10, "Luther" => -30, "Louis" => 40}), reunion.breakout 
+  end
+
 
 end
