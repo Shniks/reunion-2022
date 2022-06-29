41d4a3891c1755b095a9d556364141bc6b0e7bfc
Nikhil Shahi <shniks@gmail.com>
2022-06-29 05:07:25 -0700

    Created method for the amount owed.

--- lib/activity.rb
+++ lib/activity.rb
@@ -3,23 +3,32 @@ require 'pry'
 class Activity
 
   attr_reader :name,
-              :participants
+              :participants,
+              :amount_owed
 
   def initialize(activity)
     @name = activity
     @participants = {}
+    @amount_owed = {}
   end
 
   def add_participant(participant, cost)
-    @participants[participant] = cost
+    participants[participant] = cost
   end
 
   def total_cost
-    @participants.values.sum
+    participants.values.sum
   end
 
   def split
-    total_cost / @participants.keys.count
+    total_cost / participants.keys.count
+  end
+
+  def owed
+    participants.each do |participant, paid|
+      amount_owed[participant] = (split - paid)
+    end
+    amount_owed
   end
 
 end
