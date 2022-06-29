7b382b87a657e4a449cf3f31a0a50a6827d8ac2c
Nikhil Shahi <shniks@gmail.com>
2022-06-29 10:45:50 -0700

    Created a test to provide summary of breakout by participant.

--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -66,8 +66,22 @@ class ReunionTest < Minitest::Test
     activity_2.add_participant("Louis", 0)
     reunion.add_activity(activity_2)
 
-    assert_equal ({"Maria" => -10, "Luther" => -30, "Louis" => 40}), reunion.breakout 
+    assert_equal ({"Maria" => -10, "Luther" => -30, "Louis" => 40}), reunion.breakout
   end
 
+  def test_if_it_can_provide_summary_of_breakout_per_participant
+    reunion = Reunion.new("1406 BE")
+    activity_1 = Activity.new("Brunch")
+    activity_1.add_participant("Maria", 20)
+    activity_1.add_participant("Luther", 40)
+    reunion.add_activity(activity_1)
+    activity_2 = Activity.new("Drinks")
+    activity_2.add_participant("Maria", 60)
+    activity_2.add_participant("Luther", 60)
+    activity_2.add_participant("Louis", 0)
+    reunion.add_activity(activity_2)
+
+    assert_equal "Maria: -10\nLuther: -30\nLouis: 40", reunion.summary
+  end
 
 end
